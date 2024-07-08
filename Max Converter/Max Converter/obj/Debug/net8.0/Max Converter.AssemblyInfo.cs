bfa59b377f2c576ecf10c565fc507d0d95dc36d8
Fred G <geneau7a2@gmail.com>
2024-07-08 14:02:28 -0400

    updated build

--- Max Converter/Max Converter/obj/Debug/net8.0/Max Converter.AssemblyInfo.cs
+++ Max Converter/Max Converter/obj/Debug/net8.0/Max Converter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Max Converter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8313c971c26153a381eb7f2fc47b7c552542dc00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Max Converter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Max Converter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
